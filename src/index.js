8b59fc9947d52089083a1f218d59ab1cd786fadd
lavaxcii <lavaxcii@gmail.com>
2022-09-24 14:23:01 +0200

    Try to configure things to publish on github pages.

--- src/index.js
+++ src/index.js
@@ -2,58 +2,12 @@ import './styleV.css';
 import gameLoop from './mainGameLoop';
 import uiElements from './uiElements';
 
-// window.aiGameBoardSqrs = gameLoop.aiPlayer.gameBoard.gameBoardSqrs
-// window.playerGameBoardSqrs = gameLoop.humanPlayer.gameBoard.gameBoardSqrs
 window.gameLoop = gameLoop
 uiElements.createElement('div', 'mainMenuContainer', null, '.content', 1);
 uiElements.createElement('button', 'startBtn', null, '.mainMenuContainer', 1);
 uiElements.textContentForElement('.startBtn', 'START GAME');
 gameLoop.generateGameElements();
 
-// gameLoop.message();
-
-// const playerGameBoard = GameBoard(); 
-// const aiGameBoard = GameBoard();
-
-// playerGameBoard.generateShips();
-// console.table(playerGameBoard.shipsData)
-// playerGameBoard.checkSunkStatus();
-// console.table(playerGameBoard.mediumShipsData)
-// console.table(playerGameBoard.largeShipsData)
-
-// playerGameBoard.generateSquares();
-// console.table(playerGameBoard.gameBoardSqrs)
-// gameLoop.aiPlayer.gameBoard.placeShipOnSqr(11, 'east', 'SmallShip')
-// gameLoop.aiPlayer.gameBoard.placeShipOnSqr(14, 'east', 'SmallShip')
-// gameLoop.aiPlayer.gameBoard.placeShipOnSqr(17, 'east', 'SmallShip')
-// gameLoop.aiPlayer.gameBoard.placeShipOnSqr(21, 'east', 'SmallShip')
-
-// gameLoop.aiPlayer.gameBoard.placeShipOnSqr('13')
-// gameLoop.aiPlayer.gameBoard.placeShipOnSqr('15')
-// gameLoop.aiPlayer.gameBoard.placeShipOnSqr('16')
-// gameLoop.aiPlayer.gameBoard.placeShipOnSqr('17')
-// gameLoop.aiPlayer.gameBoard.placeShipOnSqr('18')
-// gameLoop.aiPlayer.gameBoard.placeShipOnSqr('110')
-// gameLoop.aiPlayer.gameBoard.placeShipOnSqr('21')
-
-// gameLoop.aiPlayer.gameBoard.recieveAttack(21)
-// gameLoop.aiPlacementLoop();
-// gameLoop.playerPlacementLoop();
-
-// console.table(gameLoop.aiPlayer.gameBoard.gameBoardSqrs)
-// console.table(gameLoop.humanPlayer.gameBoard.gameBoardSqrs)
-
-// gameLoop.aiPlayer.gameBoard.recieveAttack(26)
-
-// console.table(gameLoop.humanPlayer)
-// gameLoop.aiPlayer.gameBoard.recieveAttack(11)
-
-// console.table(gameLoop.aiPlayer.gameBoard.gameBoardSqrs)
-// console.table(gameLoop.aiPlayer.gameBoard.shipsData)
-
-// console.log(gameLoop.aiPlayer.determineShipPlacementForAi(gameLoop.aiPlayer.rndNrAi(10, 1)))
-// console.log(gameLoop.aiPlayer.determineShipPlacementForAi(gameLoop.aiPlayer.rndNrAi(10, 1)))
-// console.log(gameLoop.aiPlayer.determineShipPlacementForAi(gameLoop.aiPlayer.rndNrAi(10, 1)))
 
 
 
