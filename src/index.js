2ba9bb2d6a5d07e1b9e3efde017e3f26c1c23b48
lavaxcii <lavaxcii@gmail.com>
2022-09-24 14:44:21 +0200

    Add changes.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import './styleV.css';
 import gameLoop from './mainGameLoop';
 import uiElements from './uiElements';
 
-window.gameLoop = gameLoop
+// window.gameLoop = gameLoop
 uiElements.createElement('div', 'mainMenuContainer', null, '.content', 1);
 uiElements.createElement('button', 'startBtn', null, '.mainMenuContainer', 1);
 uiElements.textContentForElement('.startBtn', 'START GAME');
 gameLoop.generateGameElements();
-
-
-
 
 
